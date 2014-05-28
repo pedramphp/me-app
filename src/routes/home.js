a2afcbfc022d525abceeff0b81aed0e035d77ab7
Mahdi Pedramrazi <mpedramrazi@ebay.com>
2014-05-28 01:49:49 -0700

    no need for is mobile anymore

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,23 +1,16 @@
 'use strict';
 
 module.exports = function(req, res){
-	var ua = req.headers['user-agent'],
-		isMobile = false;
 
-	if (/mobile/i.test(ua)){
-    	isMobile = true;
-	}
 
 	res.render('pages/home', {
-	        isDev: process.env.NODE_ENV === "development",
-			title: 'About Me',
-			data: {
-				title: 'this is a title'
-			},
-			helpers:{},
-	        layout: 'main',
-	        isMobile: isMobile,
-	        isDesktop: !isMobile
+        isDev: process.env.NODE_ENV === "development",
+		title: 'About Me',
+		data: {
+			title: 'this is a title'
+		},
+		helpers:{},
+        layout: 'main'
 	});
 
 };
