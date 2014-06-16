e051c6747741f9f7e304dc3f2e3066850ceb399c
Mahdi Pedramrazi <mpedramrazi@ebay.com>
2014-06-16 01:26:24 -0700

    adding user model

---
+++ src/models/user-model.js
@@ -0,0 +1,21 @@
+var mongoose =  require('mongoose');
+var config = require('config/login-config').getConfig();
+
+mongoose.connect(config.DB.URL);
+
+var userSchema =  new mongoose.Schema({
+	fbId: String,
+	name: String,
+	email: {
+		type: String,
+		lowercase: true
+	},
+	first_name: {
+		type: String,
+		lowercase: true
+	}
+});
+
+var UserModel = mongoose.model('User', userSchema);
+
+module.exports = UserModel;
