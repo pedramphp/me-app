2cbdc2c01337400ae7898db4f37ce080650b677c
Mahdi Pedramrazi <mpedramrazi@ebay.com>
2015-02-26 22:07:00 -0800

    adding helpers for facebook controller.

--- src/controllers/timeline.js
+++ src/controllers/timeline.js
@@ -19,7 +19,46 @@ var timelineModule = function(req){
 			timeline: {
 				feed:[]
 			}
-		}
+		},
+		helpers:{	
+			isFb: function(type){
+				return type === "fb";
+			},
+
+	        fbStatusMsg: function(msg){
+				if(msg){
+					return msg.replace(/#(\S*)/g, function(match, p1, offset, string){
+						return "<a href='https://www.facebook.com/hashtag/" + p1 + "?source=feed_text' target='_blank' >"+ match +"</a>";
+					});
+				}else{
+					return "";
+				}
+	        },
+
+	        fbPostImage: function(img){
+	            if(!img){
+	                return "";
+	            }
+	            return img.replace("_s.jpg","_n.jpg");
+	        },
+
+	        fbCommentMoreTotal: function(totalComment){
+	            totalComment = totalComment - 4;
+	            if(totalComment < 0){
+	                return 0;
+	            }
+	            return totalComment;
+	        },
+
+	        igMsg: function(msg){
+				if(!msg){
+					return "";
+				}
+				return msg.replace(/@(\S*)/g, function(match, p1, offset, string){
+					return "<a href='http://instagram.com/" + p1 +"' target='_blank' >"+ match +"</a>";
+				});
+	        }
+		},
 	};
 
 	var core = {
@@ -62,7 +101,7 @@ var timelineModule = function(req){
 
 		error: function(err){
 			logger.error(err);
-			this.reject(response)
+			this.reject(response);
 		}
 	};
 	return core;
